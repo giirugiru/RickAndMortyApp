1c884071be47d450e9e74e87139a29d341e234cd
Gilang <giirugiru@gmail.com>
2023-04-15 18:05:38 +0700

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Gilang-M1Pro on 15/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
